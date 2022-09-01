e63a17c6d04c6384404052d01761e0cfc5510d56
Danilo Santos <danilocarsan@gmail.com>
2022-09-01 20:57:31 -0300

    criando campos de altura e peso

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 
+
+import { useState } from 'react';
 import styles from './App.module.css'
 import poweredImage from './assets/powered.png'
 
 const App = () => {
+
+  const [hightField, setHightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+
+  const handlerCaculateButton = () => {
+    const result = weightField / Math.pow(hightField, 2);
+    alert(result);
+  }
+
   return (
     <div className={styles.main}>
       <header>
@@ -12,7 +23,27 @@ const App = () => {
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          ...
+          <h1>Calcule seu IMC</h1>
+          <p>IMC é a sigla para o índice de massa corpórea, parâmetro adotado pela Organização Mundial da Sáude para calcular o peso ideal de cada pessoa.</p>
+
+          <input
+            placeholder="Digite sua altura em metros"
+            type="number"
+            value={hightField > 0 ? hightField : ''}
+            onChange={(v) => setHightField(parseFloat(v.target.value))}
+          />
+
+          <input
+            type="number"
+            placeholder="Digite seu peso em quilogramas"
+            value={weightField > 0 ? weightField : ''}
+            onChange={(v) => setWeightField(parseFloat(v.target.value))}
+          />
+
+          <button disabled={!(hightField > 0 && weightField > 0)} onClick={handlerCaculateButton}>
+            Calcular
+          </button>
+
         </div>
         <div className={styles.rightSide}>
           ...
