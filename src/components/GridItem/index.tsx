985f864fc64dfec50a6d984890c46a08a0b699c7
Danilo Santos <danilocarsan@gmail.com>
2022-09-01 22:23:51 -0300

    criand grid itens e estilizando

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,13 +1,25 @@
 import { level } from '../../helpers/imc';
+import styles from './styles.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
 
 type Props = {
     item: level;
 }
 
 const GridItem = ({ item }: Props) => {
-
     return (
-        <div>{item.title}</div>
+        <div className={styles.main} style={{ backgroundColor: item.color }}>
+            <div className={styles.gridIcon}>
+                <img src={item.icon === 'up' ? upImage : downImage} alt={item.icon} width={30} />
+            </div>
+            <div className={styles.gridTitle}>{item.title}</div>
+            <div className={styles.gridInfo}>
+                <>
+                    IMC está entre <strong>{item.imc[0]} Kg</strong> e <strong> {item.imc[1]} Kg </strong>
+                </>
+            </div>
+        </div>
     );
 }
 
